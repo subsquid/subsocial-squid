1076d293e466dbb32d61bc0ac1278d50c2f9a79e
mckrava <mckrava2@gmail.com>
2022-09-07 11:38:46 +0300

    fix: reduce batch size to 20

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,7 @@ export const ipfsReadOnlyNodeUrl =
 
 export const batchSize = process.env.BATCH_SIZE
   ? parseInt(process.env.BATCH_SIZE)
-  : 30;
+  : 20;
 export const chainNode =
   process.env.CHAIN_NODE || 'wss://para.f3joule.space';
 export const indexerEndpointUrl =
